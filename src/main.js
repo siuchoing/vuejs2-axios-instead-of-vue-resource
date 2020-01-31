86d987ec733a3d749c68a33821f4011c3bbe81f4
altantic <15200582@life.hlkbu.edu.hk>
2020-02-01 06:23:55 +0800

    Use interceptors to modify the request/response right before/after it is sent/recieved

--- src/main.js
+++ src/main.js
@@ -11,6 +11,26 @@ axios.defaults.baseURL = 'https://vuejs2-axios.firebaseio.com/'   // Reference b
 axios.defaults.headers.common['Authorization'] = 'admin'
 axios.defaults.headers.get['Accepts'] = 'application/json'
 
+// Use local function to return config request, otherwise it will block the request
+const reqInterceptor = axios.interceptors.request.use(config => {
+  console.log('Request Interceptor', config)
+  // config.headers['SOMETING']
+  return config
+})
+
+// Use local function to return config response,
+// otherwise otherwise other code in my application which waits for it will never receive it.
+const resInterceptor = axios.interceptors.response.use(res => {
+  console.log('Response Interceptor', res)
+  return res
+})
+
+
+// Eject needs to get the ID of the interceptor and that ID is actually returned from the use method
+// we shouldn't see any console logs in our application anymore, after eject the interceptors
+// except own console log
+axios.interceptors.request.eject(reqInterceptor);
+axios.interceptors.response.eject(resInterceptor);
 
 new Vue({
   el: '#app',
