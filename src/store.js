ad945451bbb3fcdc914b066145c5c9befc1090c8
altantic <15200582@life.hlkbu.edu.hk>
2020-02-01 12:12:00 +0800

    Accessing other Resources from Vuex, like globalAxis
    1. [Firebase Server] Develop > Database > Rules > Set value of write as "auth != null"

--- src/store.js
+++ src/store.js
@@ -1,21 +1,24 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from './axios-auth';
-
+import globalAxios from 'axios';
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
     idToken: null,
-    userId: null
+    userId: null,
+    user: null
   },
   mutations: {
     // change the state to store Auth Data in Vuex
     authUser (state, userData) {
       state.idToken = userData.token
       state.userId = userData.userId
+    },
+    storeUser (state, user) {
+        state.user = user
     }
-
   },
   actions: {
     // Sending payload object for Auth requests by vuex
@@ -32,6 +35,8 @@ export default new Vuex.Store({
               token: res.data.idToken,
               userId: res.data.localId
             })
+              //passing all data
+              dispatch('storeUser', authData)
           })
           .catch(error => console.log(error))
     },
@@ -50,9 +55,33 @@ export default new Vuex.Store({
             })
           })
           .catch(error => console.log(error))
+    },
+    storeUser ({commit}, userData) {
+       globalAxios.post('/users.json', userData)
+           .then(res => console.log)
+           .catch(error => console.log(error))
+    },
+    fetchUser ({commit}) {
+        globalAxios.get('/user.json')
+          .then(res => {
+              console.log(res)
+              const data = res.data
+              const users = []
+              for (let key in data) {
+                  const user = data[key]
+                  user.id = key
+                  users.push(user)
+              }
+              console.log(users)
+              commit('storeUser', users[0])
+              this.email = users[0].email
+          })
+          .catch(error => console.log(error))
     }
   },
   getters: {
-
+    user (state) {
+        return state.user
+    }
   }
 })
